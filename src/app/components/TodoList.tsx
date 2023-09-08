51bf69d67b740be1749e2b4538cc9abf92146502
Shinzo Nakaoka <shinzonakaoka@ShinzonoMacBook-Air.local>
2023-09-08 16:38:07 +0900

    update : todoアプリ完成

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import { Task } from "@/types";
 
-const TodoList = () => {
+import React from "react";
+import Todo from "./Todo";
+
+interface TodoListProps {
+  todos: Task[];
+}
+
+const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className="space-y-3">
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+      {/* <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
         <span>散歩</span>
         <div>
           <button className="text-green-500 mr-3">edit</button>
           <button className="text-red-500">delete</button>
         </div>
-      </li>
+      </li> */}
     </ul>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+import { getAllTodos } from "@/api";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
 
-export default function Home() {
+export default async function Home() {
+
+  const todos = await getAllTodos();
+  console.log(todos);
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
       <h1 className="text-4xl font-bold text-gray-700 -mt-32">Nextjs 13 Todo app</h1>
       <div className="w-full max-w-xl items-center mt-5">
         <div className="w-full px-8 py-6 bg-white shadow-md">
           <AddTask />
-          <TodoList />
+          <TodoList todos={todos} />
         </div>
       </div>
     </main>

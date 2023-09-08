416f79cc808d407a25a50f15f9d27313b82a7d80
Shinzo Nakaoka <shinzonakaoka@ShinzonoMacBook-Air.local>
2023-09-08 13:55:00 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className="text-4xl font-bold text-gray-700 -mt-32">Nextjs 13 Todo app</h1>
+      <div className="w-full max-w-xl items-center mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md">
+          <AddTask />
+          <TodoList />
+        </div>
+      </div>
+    </main>
+  );
+}
